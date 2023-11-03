ae0aeb979eb03d23f05d5274bc52c6d406917131
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2023-11-02 19:50:11 -0600

    Nuevo boton menu en la pantalla instrucciones

--- Proyecto fase2/Instrucciones.java
+++ Proyecto fase2/Instrucciones.java
@@ -25,5 +25,9 @@ public class Instrucciones extends World
         GameSB sGame = new GameSB();
         addObject(sGame, 250, 117);
         sGame.setLocation(465,450);
+        
+        BotonMenu dog = new BotonMenu();
+        addObject(dog,506,314);
+        dog.setLocation(100,530);
     }
 }
